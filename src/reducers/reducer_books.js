7fd28a6f2ccd64e46629d398be891a4e9caafc95
Takamichi-tsutsumi <1220t.takamichi@gmail.com>
2016-02-02 18:41:48 +0900

    connect react & redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Takamichi on 2/2/16.
+ */
+
+export default function() {
+    return [
+        {title: 'Javascript: Good Parts'},
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'},
+    ];
+}
